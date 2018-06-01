31a9925318f93b24906cb3e497c0cf70f3c82372
YNataly <barboskinzz@gmail.com>
2018-06-01 21:49:31 +0300

    feat: Add icon for desktop browser

--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@
 
 (function() {
   const cacheName = 'restaurant-rev-cache-v4';
-  const imgsCache = 'restaurant-rev-imgs-v3';
-
-  const currentChaches = [cacheName, imgsCache];
+  const imgsCache = 'restaurant-rev-imgs-v4';
   //
+  const currentChaches = [cacheName, imgsCache];
+
   const defaultRestaurantImg='img/restaurant.svg';
-  const icons=['img/restaurants-icon-192.png', 'img/restaurants-icon-512.png'];
+  const icons=['img/restaurants-icon-48.png', 'img/restaurants-icon-192.png', 'img/restaurants-icon-512.png'];
   const defaultRestaurantImgName=defaultRestaurantImg.match(/^.*\/([^/]+)\.svg$/i)[1];
 
   self.addEventListener('install', function (event) {
